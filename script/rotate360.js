ed07b700ce2fbceb20b3520333f8bdf3966aa3db
jhh <jhh@kobstaden.dk>
2012-08-08 07:03:12 -0700

    first commit

---
+++ script/rotate360.js
@@ -0,0 +1,104 @@
+/*
+    rotate360 v0.1
+    by Rickyh.co.uk & Oliveta
+    powered by Mootools
+*/
+var Rotate360 = new Class({
+    Implements: [Options],
+    options: {
+        autoPlay: false,
+        tickAmount: 10,
+        frameTime: 1000,
+        firstFrame: 0
+    },
+    initialize: function (data, target, options) {
+        this.setOptions(options);
+        if (this.options.speed < 1 || this.options.speed > 10) this.options.speed = 5;
+        
+        this.imageAmount = data.length;
+        this.mdown = false;
+        this.posun_x = 0;
+        this.posXori = 0;
+
+        if (this.options.firstFrame > 0 && this.options.firstFrame < this.imageAmount) {
+            this.i = this.options.firstFrame - 1;
+            this.nextImage();
+        } else this.i = 0;
+
+        var self = this;
+
+        this.wrapper = new Element("div").inject(target);
+        
+        this.images360 = new Asset.images(data, {
+            onComplete:function(){
+                if (!self.options.autoPlay) {
+                    self.wrapper.addEvents({
+                        'mousedown': function (e) {
+                            e.stop();
+                            self.options.autoPlay = false;
+                            self.mdown = true;
+                            self.posXori = e.page.x;
+                        },
+                        'mouseup': function (e) {
+                            self.mdown = false;
+                        },
+                        'mouseleave': function (e) {
+                            //self.mdown = false;
+                        },
+                        'mousemove': function (e) {
+                            e.stop();
+                            if (self.mdown) {
+                                if (Math.abs(self.posXori - e.page.x) >= self.options.tickAmount) {
+                                    if (self.posXori - e.page.x >= self.options.tickAmount) {
+                                        self.posXori = e.page.x;
+                                        self.moveForward();
+                                    } else {
+                                        self.posXori = e.page.x;
+                                        self.moveBack();
+                                    }
+                                }
+                            }
+
+                        }
+                    }); //end addEvents
+                } else {
+                    self.nextImage.periodical(self.options.frameTime, self);
+                }
+                self.draw();
+            },
+            onProgress:function(counter, index, source){
+                if(index === 0){
+                    self.activeImageIndex = 0;
+                    self.draw();
+                }
+                //TODO progress
+            }
+        });
+
+    },
+    draw: function(){
+        this.wrapper.empty();
+        this.images360[this.activeImageIndex].inject(this.wrapper);
+    },
+    nextImage: function () {
+        this.activeImageIndex++;
+        if (this.activeImageIndex == this.imageAmount) {
+            this.activeImageIndex = 0;
+        }
+        this.draw();
+    },
+    moveForward: function () {
+        this.activeImageIndex++;
+        if (this.activeImageIndex == this.imageAmount) {
+            this.activeImageIndex = 0;
+        }
+        this.draw();
+    },
+    moveBack: function () {
+        this.activeImageIndex--;
+        if (this.activeImageIndex == -1) {
+            this.activeImageIndex = this.imageAmount - 1;
+        }
+        this.draw();
+    }
+});
